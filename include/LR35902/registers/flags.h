8ab1e6b52d9b6c30733c45a1fd6ddadc32db88e6
vanilla milkshake <vanilla.milkshake.for.life@gmail.com>
2022-05-10 15:27:48 +0300

    Use struct for flags type, nothing to hide

--- include/LR35902/registers/flags.h
+++ include/LR35902/registers/flags.h
@@ -6,15 +6,13 @@ namespace LR35902 {
 
 // https://gbdev.io/pandocs/CPU_Registers_and_Flags.html#the-flags-register-lower-8-bits-of-af-register
 
-class flags {
-public:
+struct flags {
   bool z = false;
   bool n = false;
   bool h = false;
   bool c = false;
 
-  byte data() const noexcept {
-    // 0bZNHC'0000
+  byte data() const noexcept { // 0bZNHC'0000
     return (z << 7 | n << 6 | h << 5 | c << 4);
   }
 };
