19645725e4607fdb1ea1783c297076e9acda4c09
vanilla milkshake <adembudak@outlook.com>
2022-05-25 00:29:55 +0300

    Make user defined literals consteval

--- include/LR35902/config.h
+++ include/LR35902/config.h
@@ -6,16 +6,17 @@
 namespace LR35902 {
 
 using byte = std::uint8_t;
+using word = std::uint16_t;
 
-inline unsigned long long operator"" _B(const unsigned long long l) { //
+consteval unsigned long long operator"" _B (const unsigned long long l) { //
   return l;
 }
 
-inline unsigned long long operator"" _KiB(const unsigned long long l) {
+consteval unsigned long long operator"" _KiB (const unsigned long long l) {
   return 1024 * l;
 }
 
-inline unsigned long long operator"" _MiB(const unsigned long long l) {
+consteval unsigned long long operator"" _MiB (const unsigned long long l) {
   return 1024_KiB * l;
 }
 
