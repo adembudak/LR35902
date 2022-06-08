e34b7509eb95d83e4e8662aedc2612dff8ddc380
adem budak <adembudak@outlook.com>
2022-06-08 17:11:48 +0300

    Remove redundant headers

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,10 +1,6 @@
 #include "LR35902/core.h"
 #include "LR35902/debugView/debugView.h"
-
-#include <filesystem>
-#include <fstream>
-#include <string_view>
-#include <vector>
+#include "LR35902/stubs/cartridge/cartridge.h"
 
 #include <SDL2/SDL.h>
 #include <imgui_impl_sdl.h>
@@ -21,19 +17,17 @@ int main(int argc, char **argv) {
     DebugView debugger;
 
     bool done = false;
-
     while(!done) {
       cpu.run();
+
       SDL_Event event;
       while(SDL_PollEvent(&event)) {
         ImGui_ImplSDL2_ProcessEvent(&event);
-
         if(event.type == SDL_QUIT) done = true;
       }
 
       debugger.show(cpu);
     }
-    return 0;
   }
 
   return 1;
