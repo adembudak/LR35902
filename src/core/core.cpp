a22eae34c7e503c49a6efffdd5fb83265cd000db
adem budak <adembudak@outlook.com>
2022-06-22 12:19:22 +0300

    Implemented instruction: dec SP
    https://rgbds.gbdev.io/docs/v0.5.2/gbz80.7/#DEC_SP

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -825,8 +825,14 @@ void Core::rst(const std::size_t v) noexcept {};        // rst vec
 // // Stack Operations Instructions
 void Core::add(HL_register_tag, SP_register_tag) noexcept {}; // add HL,SP // "-" "0" "H" "C"
 void Core::add(SP_register_tag, const e8 e) noexcept {};      // add SP,e8 // "0" "0" "H" "C"
-void Core::dec(SP_register_tag) noexcept {};                  // dec SP
-void Core::inc(SP_register_tag) noexcept {};                  // inc SP
+
+void Core::dec(SP_register_tag) noexcept { // dec SP
+  --SP.m_data;
+
+  m_clock.cycle(2);
+}
+
+void Core::inc(SP_register_tag) noexcept {}; // inc SP
 
 void Core::ld(SP_register_tag, const n16 nn) noexcept {};                // ld SP,n16
 void Core::ld(byte &b, SP_register_tag) noexcept {};                     // ld [n16],SP
