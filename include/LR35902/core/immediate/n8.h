62aca2fed0d2b1467b2e41d245ded01004868d13
adem budak <adembudak@outlook.com>
2022-06-27 23:57:14 +0300

    Added lowNibble()/highNibble() for n8

--- include/LR35902/core/immediate/n8.h
+++ include/LR35902/core/immediate/n8.h
@@ -6,6 +6,14 @@ namespace LR35902 {
 
 struct n8 {
   std::uint8_t m_data{};
+
+  [[nodiscard]] std::uint8_t lowNibble() const noexcept {
+    return m_data & 0b0000'1111;
+  }
+
+  [[nodiscard]] std::uint8_t highNibble() const noexcept {
+    return (m_data & 0b1111'0000) >> 4;
+  }
 };
 
 } // namespace LR35902
