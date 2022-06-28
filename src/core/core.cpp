cecb1239da3a70b9d9c040dc7d3ef9eaf9471c29
adem budak <adembudak@outlook.com>
2022-06-28 15:57:26 +0300

    Implemented instruction: dec r8
    https://rgbds.gbdev.io/docs/v0.5.2/gbz80.7/#DEC_r8

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -730,8 +730,14 @@ void Core::cp(const n8 n) noexcept { // cp A,n8
   m_clock.cycle(2);
 }
 
-void Core::dec(r8 &r) noexcept {};   // dec r8 //    // "Z" "1" "H" "-"
 void Core::dec(byte &b) noexcept {}; // dec [HL] // "Z" "1" "H" "-"
+void Core::dec(r8 &r) noexcept { // dec r8 //    // "Z" "1" "H" "-"
+  --r;
+
+  F = {r == 0, 1, r.lowNibble() == 0b0000'1111, F.c};
+
+  m_clock.cycle(1);
+}
 
 void Core::inc(r8 &r) noexcept {};   // inc r8 //  // "Z" "0" "H" "-"
 void Core::inc(byte &b) noexcept {}; // inc [HL]
