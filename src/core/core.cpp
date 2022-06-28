3bbc97f27f93e31efa819e83f2ff6ea9e106beaa
adem budak <adembudak@outlook.com>
2022-06-28 17:15:21 +0300

    Implemented instruction: add HL, r16
    https://rgbds.gbdev.io/docs/v0.5.2/gbz80.7/#ADD_HL,r16

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -858,9 +858,19 @@ void Core::xor_(const n8 n) noexcept { // xor A,n8
 }
 
 // // 16-bit Arithmetic Instructions
-void Core::add(HL_register_tag, const r16 rr) noexcept {}; // add HL,r16  // "-" "0" "H" "C"
-void Core::dec(r16 &rr) noexcept {};                       // dec r16
-void Core::inc(r16 &rr) noexcept {};                       // inc r16
+void Core::add(HL_register_tag, const r16 rr) noexcept { // add HL,r16  // "-" "0" "H" "C"
+  const flag c = (HL.data() + rr.data()) > r16::max();
+  const flag h = ((HL.data() & 0x0fff) + (rr.data() & 0x0fff)) > 0x0fff;
+
+  HL += rr;
+
+  F = {F.z, 0, h, c};
+
+  m_clock.cycle(2);
+}
+
+void Core::dec(r16 &rr) noexcept {}; // dec r16
+void Core::inc(r16 &rr) noexcept {}; // inc r16
 
 // // Bit Operations Instructions
 void Core::bit(const u3 u, const r8 r) noexcept { // bit u3,r8 // "Z" "0" "1" "-"
