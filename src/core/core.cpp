acfe6a871839ba68f7a03c3615b17ab0888d5bc0
adem budak <adembudak@outlook.com>
2022-06-28 12:55:31 +0300

    Implemented instruction: rlc [HL]
    https://rgbds.gbdev.io/docs/v0.5.2/gbz80.7/#RLC__HL_

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -928,7 +928,6 @@ void Core::rla() noexcept { // rla // "0" "0" "0" "C"
   m_clock.cycle(1);
 }
 
-void Core::rlc(byte &b) noexcept {}; // rlc [HL]
 void Core::rlca() noexcept {};       // rlca //  // "0" "0" "0" "C"
 void Core::rlc(r8 &r) noexcept { // rlc r8 // "Z" "0" "0" "C"
                                  // C <- [7 <- 0] <- [7]
@@ -941,6 +940,18 @@ void Core::rlc(r8 &r) noexcept { // rlc r8 // "Z" "0" "0" "C"
   F = {r == 0, 0, 0, F.c};
 
   m_clock.cycle(2);
+}
+
+void Core::rlc(byte &b) noexcept { // rlc [HL]
+  const flag old_7th_bit = b & 0b1000'0000;
+  F.c = old_7th_bit;
+
+  b <<= 1;
+  b |= old_7th_bit;
+
+  F = {b == 0, 0, 0, F.c};
+
+  m_clock.cycle(4);
 }
 
 void Core::rr(r8 &r) noexcept { // rr r8 // "Z" "0" "0" "C"
