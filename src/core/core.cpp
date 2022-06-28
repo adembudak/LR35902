01cfa479f78e04c976c8b0eed08c65defc8c91ab
adem budak <adembudak@outlook.com>
2022-06-28 13:06:35 +0300

    Implemented instruction: rrc r8
    https://rgbds.gbdev.io/docs/v0.5.2/gbz80.7/#RRC_r8

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -1002,9 +1002,17 @@ void Core::rra() noexcept { // rra // // "0" "0" "0" "C"
   m_clock.cycle(1);
 }
 
-void Core::rrc(r8 &r) noexcept {};   // rrc r8 // "Z" "0" "0" "C"
 void Core::rrc(byte &b) noexcept {}; // rrc [HL]
 void Core::rrca() noexcept {};       // rrca // // "0" "0" "0" "C"
+void Core::rrc(r8 &r) noexcept { // rrc r8 // "Z" "0" "0" "C"
+                                 // [0] -> [7 -> 0] -> C
+  F.c = r.data() & 0b0000'0001;
+  r >>= 1;
+
+  F = {r == 0, 0, 0, F.c};
+
+  m_clock.cycle(2);
+}
 
 void Core::sla(r8 &r) noexcept {};   // sla r8 // "Z" "0" "0" "C"
 void Core::sla(byte &b) noexcept {}; // sla [HL]
