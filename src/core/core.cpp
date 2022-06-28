fadb31f19764c800a19459d3201400cb7965d0a0
adem budak <adembudak@outlook.com>
2022-06-28 12:38:41 +0300

    Implemented instruction: rr r8
    https://rgbds.gbdev.io/docs/v0.5.2/gbz80.7/#RR_r8

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -932,9 +932,20 @@ void Core::rlc(r8 &r) noexcept {};   // rlc r8 // "Z" "0" "0" "C"
 void Core::rlc(byte &b) noexcept {}; // rlc [HL]
 void Core::rlca() noexcept {};       // rlca //  // "0" "0" "0" "C"
 
-void Core::rr(r8 &r) noexcept {};   // rr r8 // "Z" "0" "0" "C"
 void Core::rr(byte &b) noexcept {}; // rr [HL]
 void Core::rra() noexcept {};       // rra // // "0" "0" "0" "C"
+void Core::rr(r8 &r) noexcept { // rr r8 // "Z" "0" "0" "C"
+                                // C -> [7 -> 0] -> C
+  const flag old_carry = F.c;
+  F.c = r.data() & 0b0000'0001;
+
+  r >>= 1;
+  r |= byte(old_carry << 7);
+
+  F = {r == 0, 0, 0, F.c};
+
+  m_clock.cycle(2);
+}
 
 void Core::rrc(r8 &r) noexcept {};   // rrc r8 // "Z" "0" "0" "C"
 void Core::rrc(byte &b) noexcept {}; // rrc [HL]
