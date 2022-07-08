79cb36966fe7741f570edd4fb5573d9504e5524a
adem budak <adembudak@outlook.com>
2022-07-08 16:25:26 +0300

    Added memory bank controller (mbc) kind enumaration

--- include/LR35902/stubs/cartridge/cartridge.h
+++ include/LR35902/stubs/cartridge/cartridge.h
@@ -12,6 +12,39 @@ private:
   std::vector<byte> m_rom{};
   std::array<byte, 8_KiB> m_sram{};
 
+  enum class mbc : std::uint8_t {
+    // clang-format off
+    rom_only                       = 0x00,
+    mbc1                           = 0x01,
+    mbc1_ram                       = 0x02,
+    mbc1_ram_battery               = 0x03,
+    mbc2                           = 0x05,
+    mbc2_battery                   = 0x06,
+    rom_ram                        = 0x08,
+    rom_ram_battery                = 0x09,
+    mmm01                          = 0x0b,
+    mmm01_ram                      = 0x0c,
+    mmm01_ram_battery              = 0x0d,
+    mbc3_timer_battery             = 0x0f,
+    mbc3_timer_ram_battery         = 0x10,
+    mbc3                           = 0x11,
+    mbc3_ram                       = 0x12,
+    mbc3_ram_battery               = 0x13,
+    mbc5                           = 0x19,
+    mbc5_ram                       = 0x1a,
+    mbc5_ram_battery               = 0x1b,
+    mbc5_rumble                    = 0x1c,
+    mbc5_rumble_ram                = 0x1d,
+    mbc5_rumble_ram_battery        = 0x1e,
+    mbc6                           = 0x20,
+    mbc7_sensor_rumble_ram_battery = 0x22,
+    pocketCamera                   = 0xfc,
+    bandaiTama5                    = 0xfd,
+    huc3                           = 0xfe,
+    huc1_ram_battery               = 0xff
+    // clang-format on
+  };
+
 public:
   void load(const char *romfile);
 
