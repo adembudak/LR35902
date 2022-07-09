55ebbc60b9751a0d6acba47fd97e8ee20f575c77
adem budak <adembudak@outlook.com>
2022-07-09 17:44:50 +0300

    Make UDLs noexcept
    Added random_byte() function

--- include/LR35902/config.h
+++ include/LR35902/config.h
@@ -2,6 +2,7 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <random>
 
 namespace LR35902 {
 
@@ -9,16 +10,22 @@ using flag = bool;
 using byte = std::uint8_t;
 using word = std::uint16_t;
 
-[[nodiscard]] consteval unsigned long long operator"" _B(const unsigned long long l) {
+[[nodiscard]] consteval unsigned long long operator"" _B(const unsigned long long l) noexcept {
   return l;
 }
 
-[[nodiscard]] consteval unsigned long long operator"" _KiB(const unsigned long long l) {
+[[nodiscard]] consteval unsigned long long operator"" _KiB(const unsigned long long l) noexcept {
   return 1024 * l;
 }
 
-[[nodiscard]] consteval unsigned long long operator"" _MiB(const unsigned long long l) {
+[[nodiscard]] consteval unsigned long long operator"" _MiB(const unsigned long long l) noexcept {
   return 1024_KiB * l;
+}
+
+inline std::mt19937 engine{std::random_device{}()};
+inline std::uniform_int_distribution get{0, 255};
+[[nodiscard]] inline byte random_byte() noexcept {
+  return byte(get(engine));
 }
 
 } // namespace LR35902
