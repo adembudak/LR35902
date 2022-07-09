6a288550dfbc5ff98977453bdfb9c22a672d17e0
adem budak <adembudak@outlook.com>
2022-07-09 23:11:49 +0300

    Implemented read/write rom/sram functions.
    Store rom/ram size with string
    Moved mbc enumaration to source file

--- include/LR35902/stubs/cartridge/cartridge.h
+++ include/LR35902/stubs/cartridge/cartridge.h
@@ -4,54 +4,23 @@
 #include <LR35902/stubs/cartridge/kind/rom_only.h>
 #include <LR35902/stubs/cartridge/kind/rom_ram.h>
 
+#include <cstdint>
 #include <variant>
 
 namespace LR35902 {
+enum class mbc : std::uint8_t;
 
 // this things: https://en.wikipedia.org/wiki/ROM_cartridge#/media/File:PokemonSilverBoard.jpg
 class Cartridge {
   std::variant<rom_only, rom_ram> m_cart;
 
-  enum class mbc : std::uint8_t {
-    // clang-format off
-    rom_only                       = 0x00, // done
-    mbc1                           = 0x01,
-    mbc1_ram                       = 0x02,
-    mbc1_ram_battery               = 0x03,
-    mbc2                           = 0x05,
-    mbc2_battery                   = 0x06,
-    rom_ram                        = 0x08, // done
-    rom_ram_battery                = 0x09,
-    mmm01                          = 0x0b,
-    mmm01_ram                      = 0x0c,
-    mmm01_ram_battery              = 0x0d,
-    mbc3_timer_battery             = 0x0f,
-    mbc3_timer_ram_battery         = 0x10,
-    mbc3                           = 0x11,
-    mbc3_ram                       = 0x12,
-    mbc3_ram_battery               = 0x13,
-    mbc5                           = 0x19,
-    mbc5_ram                       = 0x1a,
-    mbc5_ram_battery               = 0x1b,
-    mbc5_rumble                    = 0x1c,
-    mbc5_rumble_ram                = 0x1d,
-    mbc5_rumble_ram_battery        = 0x1e,
-    mbc6                           = 0x20,
-    mbc7_sensor_rumble_ram_battery = 0x22,
-    pocketCamera                   = 0xfc,
-    bandaiTama5                    = 0xfd,
-    huc3                           = 0xfe,
-    huc1_ram_battery               = 0xff
-    // clang-format on
-  };
-
   struct {
     bool nintendo_logo_check{};
-    const char title[12]{}; // 11 character + '\0'
+    char title[12]{}; // 11 character + '\0'
     mbc kind{};
     bool checksum{};
-    byte rom_size{};
-    byte ram_size{};
+    std::string_view rom_size{};
+    std::string_view ram_size{};
   } CartridgeHeader;
 
 public:
@@ -67,8 +36,5 @@ public:
   [[nodiscard]] auto data() const noexcept -> decltype(auto);
 
   friend class DebugView;
-
-private:
-  void decodeCartridgeHeader();
 };
 }
--- src/stubs/cartridge/cartridge.cpp
+++ src/stubs/cartridge/cartridge.cpp
@@ -3,6 +3,7 @@
 #include <LR35902/stubs/cartridge/kind/rom_only.h>
 #include <LR35902/stubs/cartridge/kind/rom_ram.h>
 
+#include <algorithm>
 #include <cstdint>
 #include <fstream>
 #include <vector>
@@ -14,11 +15,103 @@ constexpr byte nintendo_logo[]{0xce, 0xed, 0x66, 0x66, 0xcc, 0x0d, 0x00, 0x0b, 0
                                0xdc, 0xcc, 0x6e, 0xe6, 0xdd, 0xdd, 0xd9, 0x99, 0xbb, 0xbb, 0x67, 0x63,
                                0x6e, 0x0e, 0xec, 0xcc, 0xdd, 0xdc, 0x99, 0x9f, 0xbb, 0xb9, 0x33, 0x3e};
 
+enum class mbc : std::uint8_t {
+  // clang-format off
+    rom_only                       = 0x00, // done
+    mbc1                           = 0x01,
+    mbc1_ram                       = 0x02,
+    mbc1_ram_battery               = 0x03,
+    mbc2                           = 0x05,
+    mbc2_battery                   = 0x06,
+    rom_ram                        = 0x08, // done
+    rom_ram_battery                = 0x09,
+    mmm01                          = 0x0b,
+    mmm01_ram                      = 0x0c,
+    mmm01_ram_battery              = 0x0d,
+    mbc3_timer_battery             = 0x0f,
+    mbc3_timer_ram_battery         = 0x10,
+    mbc3                           = 0x11,
+    mbc3_ram                       = 0x12,
+    mbc3_ram_battery               = 0x13,
+    mbc5                           = 0x19,
+    mbc5_ram                       = 0x1a,
+    mbc5_ram_battery               = 0x1b,
+    mbc5_rumble                    = 0x1c,
+    mbc5_rumble_ram                = 0x1d,
+    mbc5_rumble_ram_battery        = 0x1e,
+    mbc6                           = 0x20,
+    mbc7_sensor_rumble_ram_battery = 0x22,
+    pocketCamera                   = 0xfc,
+    bandaiTama5                    = 0xfd,
+    huc3                           = 0xfe,
+    huc1_ram_battery               = 0xff
+  // clang-format on
+};
+
 void Cartridge::load(const char *romfile) {
   std::ifstream fin{romfile};
   fin.exceptions(std::ifstream::failbit);
 
   std::vector<byte> dumpedGamePak(std::istreambuf_iterator<char>{fin}, {});
+
+  // https://gbdev.io/pandocs/The_Cartridge_Header.html
+  const std::size_t nintendo_logo_begin = 0x104;
+  CartridgeHeader.nintendo_logo_check = std::equal(std::cbegin(nintendo_logo), std::cend(nintendo_logo),
+                                                   dumpedGamePak.cbegin() + nintendo_logo_begin);
+
+  const std::size_t title_begin = 0x134;
+  std::copy_n(dumpedGamePak.begin() + title_begin, 11_B, std::begin(CartridgeHeader.title));
+
+  const std::size_t cartridge_type = 0x134;
+  switch(dumpedGamePak[cartridge_type]) {
+  case 0x00:
+    CartridgeHeader.kind = mbc::rom_only;
+    m_cart = rom_only(begin(dumpedGamePak), end(dumpedGamePak));
+    break;
+  case 0x08:
+    CartridgeHeader.kind = mbc::rom_ram;
+    m_cart = rom_ram(begin(dumpedGamePak), end(dumpedGamePak));
+    break;
+  };
+
+  const std::size_t checksum_begin = 0x134;
+  const std::size_t checksum_end = 0x14c + 1; // +1 for to use the address as an end iterator
+  const byte checksum_result = dumpedGamePak[0x14d];
+
+  CartridgeHeader.checksum =
+      checksum_result == std::accumulate(&dumpedGamePak[checksum_begin], &dumpedGamePak[checksum_end], 0,
+                                         [](const byte x, const byte y) { return x - y - 1; });
+
+  const byte rom_size = dumpedGamePak[0x0148];
+  CartridgeHeader.rom_size = [&rom_size]() -> std::string_view {
+    switch(rom_size) {
+    case 0x00: return "32_KiB";
+    case 0x01: return "64_KiB";
+    case 0x02: return "128_KiB";
+    case 0x03: return "256_KiB";
+    case 0x04: return "512_KiB";
+    case 0x05: return "1_MiB";
+    case 0x06: return "2_MiB";
+    case 0x07: return "4_MiB";
+    case 0x08: return "8_MiB";
+    case 0x52: return "1.1_MiB";
+    case 0x53: return "1.2_MiB";
+    case 0x54: return "1.5_MiB";
+    default: return "Failed to decode";
+    };
+  }();
+
+  const byte ram_size = dumpedGamePak[0x0149];
+  CartridgeHeader.ram_size = [&ram_size]() -> std::string_view {
+    switch(ram_size) {
+    case 0x00: return "0_KiB";
+    case 0x02: return "8_KiB";
+    case 0x03: return "32_KiB";
+    case 0x04: return "128_KiB";
+    case 0x05: return "64_KiB";
+    default: return "Failed to decode";
+    }
+  }();
 }
 
 // clang-format off
