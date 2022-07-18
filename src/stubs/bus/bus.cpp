1c7f03c5bfd529256da4c9aa2540a845dd74a87d
adem budak <adembudak@outlook.com>
2022-07-18 05:59:02 +0300

    Fixed buggy address normalization

--- src/stubs/bus/bus.cpp
+++ src/stubs/bus/bus.cpp
@@ -24,7 +24,7 @@ byte &Bus::read_write(const std::size_t index) noexcept {
   else if(index < sram_end) return m_cart[index];
   else if(index < wramx_end) return m_builtIn[index];
   else if(index < echo_end) return m_builtIn[index];
-  else if(index < oam) return m_ppu[index];
+  else if(index < oam_end) return m_ppu[index];
   else if(index < noUsable_end) return m_builtIn[index];
   else if(index < io_end) return index == 0xff0f ? interruptHandler.IF : m_io[index];
   else if(index < hram_end) return m_builtIn[index];
@@ -38,7 +38,7 @@ byte Bus::read(const std::size_t index) const noexcept {
   else if(index < sram_end) return m_cart.readSRAM(index - sram);
   else if(index < wramx_end) return m_builtIn.readWRAM(index - wram0);
   else if(index < echo_end) return m_builtIn.readEcho(index - echo);
-  else if(index < oam) return m_ppu.readOAM(index - oam);
+  else if(index < oam_end) return m_ppu.readOAM(index - oam);
   else if(index < noUsable_end) return m_builtIn.readNoUsable(index - noUsable);
   else if(index < io_end) return index == 0xff0f ? interruptHandler.IF : m_io.readIO(index - io);
   else if(index < hram_end) return m_builtIn.readHRAM(index - hram);
@@ -53,7 +53,7 @@ void Bus::write(const std::size_t index, const byte b) noexcept {
   else if(index < sram_end) m_cart.writeSRAM(index - sram, b);
   else if(index < wramx_end) m_builtIn.writeWRAM(index - wram0, b);
   else if(index < echo_end) m_builtIn.writeEcho(index - echo, b);
-  else if(index < oam) m_ppu.writeOAM(index - oam, b);
+  else if(index < oam_end) m_ppu.writeOAM(index - oam, b);
   else if(index < noUsable_end) m_builtIn.writeNoUsable(index - noUsable, b);
   else if(index < io_end) { if(index == 0xff0f) interruptHandler.IF = b; else m_io.writeIO(index, b); } 
   else if(index < hram_end) m_builtIn.writeHRAM(index - hram, b);
