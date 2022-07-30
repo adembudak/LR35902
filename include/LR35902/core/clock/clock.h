d6b6593bfcffb897b208e6de832801c3fbb01451
adem budak <adembudak@outlook.com>
2022-07-30 17:27:34 +0300

    Added getter for latest cycle

--- include/LR35902/core/clock/clock.h
+++ include/LR35902/core/clock/clock.h
@@ -6,15 +6,22 @@ namespace LR35902 {
 
 class clock {
   std::size_t m_data{};
+  std::size_t m_latest{};
 
 public:
   void cycle(const std::size_t t) {
+    m_latest = t;
+
     for(std::size_t i = 0; i < t; ++i)
       ++m_data;
   }
 
   [[nodiscard]] auto data() const noexcept -> std::size_t {
     return m_data;
+  }
+
+  [[nodiscard]] auto latest() const noexcept -> std::size_t {
+    return m_latest;
   }
 
   friend class DebugView;
