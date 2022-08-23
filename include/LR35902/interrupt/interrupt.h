e9605e82b5486ca198b7b674baa97037ac2614a4
adem budak <adembudak@outlook.com>
2022-08-23 22:38:33 +0300

    Removed copy pasta

--- include/LR35902/interrupt/interrupt.h
+++ include/LR35902/interrupt/interrupt.h
@@ -10,22 +10,21 @@ struct Interrupt {
   enum class kind : std::uint8_t { vblank, lcd_stat, timer, serial, joypad };
 
   byte IF{}; // 0xff0f, request
-  byte IE{}; // 0xffff, enable
+  byte IE{}; // 0xffff, enable (controlled by the game, not emu)
 
   [[nodiscard]] bool isThereAnAwaitingInterrupt() const noexcept {
     return 0b0001'1111 & IF & IE;
   }
 
+  // clang-format off
   [[nodiscard]] kind get() const noexcept {
-    if(IF & IE & 0b0000'0001) return kind::vblank;
+    if     (IF & IE & 0b0000'0001) return kind::vblank;
     else if(IF & IE & 0b0000'0010) return kind::lcd_stat;
     else if(IF & IE & 0b0000'0100) return kind::timer;
-    else if(IF & IE & 0b0000'1000) return kind::serial;
     else if(IF & IE & 0b0000'1000) return kind::serial;
     else if(IF & IE & 0b0001'0000) return kind::joypad;
   }
 
-  // clang-format off
   void request(const kind k) noexcept {
     switch(k) {
     case kind::vblank:   IF |= 0b0000'0001; break;
