8f6b48fb1cf63603fce98038558777d3206db0fa
adem budak <adembudak@outlook.com>
2022-08-24 00:17:58 +0300

    Small additions to VRAM diagram

--- include/LR35902/ppu/ppu.h
+++ include/LR35902/ppu/ppu.h
@@ -21,12 +21,16 @@ namespace LR35902 {
 //  [------------------)[------------------)[------------------)[---------)[----------)                
 //  0x8000              0x8800              0x9000              0x9800     0x9C00     0xA000           
 //                                                                                                     
+//  tile set or tileset: where to get from tiles
+//  tile map or tilemap: where to put the tiles
 //
 //  tile structure, based on https://www.huderlem.com/demos/gameboy2bpp.html
 //
 //  VRAM[] = {0xff, 0x00, 0x7e, 0xff, 0x85, 0x81, 0x89, 0x83, 0x93, 0x85, 0xa5, 0x8b, 0xc9, 0x97, 0x7e, 0xff, ... }
 //
 //  palette[] = {██, ▓▓, ▒▒, ░░}, background and window tiles uses BGP register for palette, objects (sprites) uses OBP0/OBP1
+//
+//  Each tile is 8x8 pixel and 2 bit per pixel format is used:
 //              = 8 x 8 x 2 bit
 //              = 128 bit
 //              = 16 byte
