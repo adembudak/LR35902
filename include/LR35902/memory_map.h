62d219b26fbae44debea0a568ccb80523ca70a5e
adem budak <adembudak@outlook.com>
2022-11-18 01:57:02 +0300

    Lowercase memory map point numbers

--- include/LR35902/memory_map.h
+++ include/LR35902/memory_map.h
@@ -15,33 +15,33 @@ inline constexpr std::size_t romx =          0x4000;
 inline constexpr std::size_t romx_end =      0x8000;
 
 inline constexpr std::size_t vram =          0x8000; // 8KB       PPU
-inline constexpr std::size_t vram_end =      0xA000;
+inline constexpr std::size_t vram_end =      0xa000;
 
-inline constexpr std::size_t sram =          0xA000; // 8KB       Cartridge
-inline constexpr std::size_t sram_end =      0xC000;
+inline constexpr std::size_t sram =          0xa000; // 8KB       Cartridge
+inline constexpr std::size_t sram_end =      0xc000;
 
-inline constexpr std::size_t wram0 =         0xC000; // 8KB       Built-in
-inline constexpr std::size_t wram0_end =     0xD000;
+inline constexpr std::size_t wram0 =         0xc000; // 8KB       Built-in
+inline constexpr std::size_t wram0_end =     0xd000;
 
-inline constexpr std::size_t wramx =         0xD000;
-inline constexpr std::size_t wramx_end =     0xE000;
+inline constexpr std::size_t wramx =         0xd000;
+inline constexpr std::size_t wramx_end =     0xe000;
 
-inline constexpr std::size_t echo =          0xE000; // 7KB+512B  Built-in
-inline constexpr std::size_t echo_end =      0xFE00;
+inline constexpr std::size_t echo =          0xe000; // 7KB+512B  Built-in
+inline constexpr std::size_t echo_end =      0xfe00;
 
-inline constexpr std::size_t oam =           0xFE00; // 160B      PPU
-inline constexpr std::size_t oam_end =       0xFEA0;
+inline constexpr std::size_t oam =           0xfe00; // 160B      PPU
+inline constexpr std::size_t oam_end =       0xfea0;
 
-inline constexpr std::size_t noUsable =      0xFEA0; // 96B       Built-in
-inline constexpr std::size_t noUsable_end =  0xFF00;
+inline constexpr std::size_t noUsable =      0xfea0; // 96B       Built-in
+inline constexpr std::size_t noUsable_end =  0xff00;
 
-inline constexpr std::size_t io =            0xFF00; // 128B      IO
-inline constexpr std::size_t io_end =        0xFF80;
+inline constexpr std::size_t io =            0xff00; // 128B      IO
+inline constexpr std::size_t io_end =        0xff80;
 
-inline constexpr std::size_t hram =          0xFF80; // 127B      Built-in
-inline constexpr std::size_t hram_end =      0xFFFF;
+inline constexpr std::size_t hram =          0xff80; // 127B      Built-in
+inline constexpr std::size_t hram_end =      0xffff;
 
-inline constexpr std::size_t IE =            0xFFFF; // 1B        Interrupts
+inline constexpr std::size_t IE =            0xffff; // 1B        Interrupts
 // -------------------------------------------------------------------------
                                              // Total:  64KB
 }
