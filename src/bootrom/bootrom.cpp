50af935fa88dc43bded0ebf28225093bdac9e177
adem budak <adembudak@outlook.com>
2022-11-17 22:09:50 +0300

    Use named constant for bootrom size

--- src/bootrom/bootrom.cpp
+++ src/bootrom/bootrom.cpp
@@ -2,17 +2,19 @@
 #include <LR35902/config.h>
 
 #include <cassert>
+#include <cstddef>
 #include <fstream>
 
 namespace LR35902 {
 
+constexpr std::size_t bootrom_size = 0x100;
+
 void bootROM::load() noexcept {
   std::ifstream fin{"bootrom.gb"};
-
   assert(fin.is_open() && "boot rom (bootrom.gb) not found\n");
 
   m_data.assign(std::istreambuf_iterator<char>{fin}, {});
-  m_data.resize(0x100);
+  m_data.resize(bootrom_size);
 }
 
 void bootROM::unmap() noexcept {
