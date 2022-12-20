354ef9185717c0fc5d3c6a6efe6c241f216384ec
adem budak <adembudak@outlook.com>
2022-12-20 17:51:07 +0300

    Remove commented code

--- src/cartridge/header/header.cpp
+++ src/cartridge/header/header.cpp
@@ -11,29 +11,6 @@ namespace LR35902 {
 
 using namespace std::string_literals;
 
-/*
-constexpr std::size_t logo_begin = 0x104;
-constexpr std::size_t logo_end = 0x133 + 1;
-constexpr std::size_t title_begin = 0x134;
-constexpr std::size_t title_end = 0x143 + 1;
-constexpr std::size_t manufacturer_code_begin = 0x13f;
-constexpr std::size_t manufacturer_code_end = 0x142 + 1;
-constexpr std::size_t color_gameboy_support = 0x143;
-constexpr std::size_t new_licensee_code0 = 0x144;
-constexpr std::size_t new_licensee_code1 = 0x145;
-constexpr std::size_t super_gameboy_support = 0x146;
-constexpr std::size_t mbc_code = 0x147;
-constexpr std::size_t rom_code = 0x148;
-constexpr std::size_t ram_code = 0x149;
-constexpr std::size_t destination_code = 0x14a;
-constexpr std::size_t old_licensee_code = 0x14b;
-constexpr std::size_t game_version = 0x14c;
-
-constexpr std::size_t checksum_begin = 0x134;
-constexpr std::size_t checksum_end = 0x14c + 1;
-constexpr std::size_t checksum_result = 0x14d;
-*/
-
 constexpr std::array<byte, mmap::logo_end - mmap::logo_begin> nintendo_logo{
     0xce, 0xed, 0x66, 0x66, 0xcc, 0x0d, 0x00, 0x0b, 0x03, 0x73, 0x00, 0x83, 0x00, 0x0c, 0x00, 0x0d,
     0x00, 0x08, 0x11, 0x1f, 0x88, 0x89, 0x00, 0x0e, 0xdc, 0xcc, 0x6e, 0xe6, 0xdd, 0xdd, 0xd9, 0x99,
