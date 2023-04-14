b41b677983f9e7e530d66dc7423814c335e0d147
adem budak <adembudak@outlook.com>
2023-04-14 19:26:43 +0300

    Renamed variable amount to numberOfBytesToTransfer

--- src/dma/dma.cpp
+++ src/dma/dma.cpp
@@ -1,5 +1,6 @@
 #include <LR35902/builtin/builtin.h>
 #include <LR35902/cartridge/cartridge.h>
+#include <LR35902/config.h>
 #include <LR35902/cpu/clock/clock.h>
 #include <LR35902/dma/dma.h>
 #include <LR35902/memory_map.h>
@@ -17,23 +18,23 @@ DMA::DMA(Cartridge &cart, PPU &ppu, BuiltIn &builtIn, Clock &clock) :
     m_builtIn{builtIn},
     m_clock{clock} {}
 
-constexpr std::size_t amount = 0xa0; // 40 * 32 bit == 160 byte
+constexpr std::size_t numberOfBytesToTransfer = 160_B; // 40 * 32 bits == 40 * 4 bytes == 160 bytes
 
 void DMA::action(const byte n) noexcept {
 
   if(const std::size_t destination = n * 0x100; destination < mmap::romx_end)
-    for(std::size_t i = 0; i < amount; ++i)
+    for(std::size_t i = 0; i < numberOfBytesToTransfer; ++i)
       m_ppu.m_oam[i] = m_cart.readROM(i);
 
   else if(destination < mmap::vram_end) //
-    std::ranges::copy_n(m_ppu.m_vram.begin(), amount, m_ppu.m_oam.begin());
+    std::ranges::copy_n(m_ppu.m_vram.begin(), numberOfBytesToTransfer, m_ppu.m_oam.begin());
 
   else if(destination < mmap::sram_end)
-    for(std::size_t i = 0; i < amount; ++i)
+    for(std::size_t i = 0; i < numberOfBytesToTransfer; ++i)
       m_ppu.m_oam[i] = m_cart.readSRAM(i);
 
   else if(destination < mmap::wramx_end) //
-    std::ranges::copy_n(m_builtIn.m_wram.begin(), amount, m_ppu.m_oam.begin());
+    std::ranges::copy_n(m_builtIn.m_wram.begin(), numberOfBytesToTransfer, m_ppu.m_oam.begin());
 
   else {
     // ignore write
