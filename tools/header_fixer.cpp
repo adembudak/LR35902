105b536d034537d0e803393a1376716d84b74dcf
adem budak <adembudak@outlook.com>
2023-05-04 01:57:50 +0300

    Implemented mbc kind setting option of header fixer

--- tools/header_fixer.cpp
+++ tools/header_fixer.cpp
@@ -4,10 +4,63 @@
 #include <CLI/App.hpp>
 #include <CLI/CLI.hpp>
 
+#include <range/v3/to_container.hpp>
+#include <range/v3/view/intersperse.hpp>
+#include <range/v3/view/join.hpp>
+#include <range/v3/view/map.hpp>
+
 #include <array>
+#include <map>
 #include <string>
 
+namespace {
+const std::map<std::string, LR35902::byte> kind{
+    {"rom_only",                       0x00},
+    {"mbc1",                           0x01},
+    {"mbc1_ram",                       0x02},
+    {"mbc1_ram_battery",               0x03},
+    {"mbc2",                           0x05},
+    {"mbc2_battery",                   0x06},
+    {"rom_ram",                        0x08},
+    {"rom_ram_battery",                0x09},
+    {"mmm01",                          0x0b},
+    {"mmm01_ram",                      0x0c},
+    {"mmm01_ram_battery",              0x0d},
+    {"mbc3_timer_battery",             0x0f},
+    {"mbc3_timer_ram_battery",         0x10},
+    {"mbc3",                           0x11},
+    {"mbc3_ram",                       0x12},
+    {"mbc3_ram_battery",               0x13},
+    {"mbc5",                           0x19},
+    {"mbc5_ram",                       0x1a},
+    {"mbc5_ram_battery",               0x1b},
+    {"mbc5_rumble",                    0x1c},
+    {"mbc5_rumble_ram",                0x1d},
+    {"mbc5_rumble_ram_battery",        0x1e},
+    {"mbc6",                           0x20},
+    {"mbc7_sensor_rumble_ram_battery", 0x22},
+    {"pocketCamera",                   0xfc},
+    {"bandaiTama5",                    0xfd},
+    {"HuC3",                           0xfe},
+    {"HuC1_ram_battery",               0xff}
+};
+
+struct MBCKindValidator : public CLI::Validator {
+  MBCKindValidator() {
+    name_ = "mbc kind validator";
+
+    func_ = [](const std::string &str) -> std::string {
+      if(!kind.contains(str)) return "Not a valid MBC type";
+      return std::string{};
+    };
+  }
+};
+
+} // end namespace
+
 int main(int argc, const char *const argv[]) {
+  const static MBCKindValidator mbc_kind_validator;
+
   using namespace LR35902;
   CLI::App app;
 
@@ -16,6 +69,7 @@ int main(int argc, const char *const argv[]) {
   try {
     std::string rom_file;
     std::string title;
+    std::string mbc_type;
     byte version;
 
     const auto rom = app.add_option("rom.gb", rom_file)->required(true)->check(CLI::ExistingFile);
@@ -23,6 +77,14 @@ int main(int argc, const char *const argv[]) {
     const auto set_title = app.add_option("--set-title", title, "Set rom title");
     const auto fix_csum = app.add_flag("--fix-csum", "Fix checksum");
     const auto set_version = app.add_option("--set-ver", version, "Set version")->check(CLI::Range(0x00, 0xff));
+    const auto set_mbc = app.add_option("--set-mbc", mbc_type)->check(mbc_kind_validator)->description([&]() {
+      std::ostringstream sout;
+      sout << "Valid values: " << '\n';
+      sout << (kind | ranges::views::keys | ranges::views::intersperse("\n") | ranges::views::join |
+               ranges::to<std::string>);
+
+      return sout.str();
+    }());
 
     app.parse(argc, argv);
 
@@ -48,19 +110,23 @@ int main(int argc, const char *const argv[]) {
       stream.seekg(mmap::csum_begin);
       stream.read(reinterpret_cast<char *>(data(buf)), size(buf));
 
-      const auto csum_result =
+      const byte csum_result =
           std::accumulate(cbegin(buf), cend(buf), byte{0}, [](byte a, byte b) { return a - b - 1; });
 
       stream.seekg(mmap::csum_result);
-      stream.write(reinterpret_cast<const char *>(&csum_result), sizeof(csum_result));
+      stream.write(reinterpret_cast<const char *>(&csum_result), sizeof(decltype(csum_result)));
     }
 
     if(*set_version) {
       stream.seekg(mmap::game_version);
-      stream.write(reinterpret_cast<const char *>(&version), sizeof(version));
+      stream.write(reinterpret_cast<const char *>(&version), sizeof(decltype(version)));
     }
 
-    //
+    if(*set_mbc) {
+      stream.seekg(mmap::mbc_code);
+      const byte code = kind.find(mbc_type)->second;
+      stream.write(reinterpret_cast<const char *>(&code), sizeof(decltype(code)));
+    }
   }
   catch(const CLI::ParseError &e) {
     return app.exit(e);
