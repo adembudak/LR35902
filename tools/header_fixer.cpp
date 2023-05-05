b7d337495b29f0b3315cb0f96680cfd76ba8535d
adem budak <adembudak@outlook.com>
2023-05-05 13:33:47 +0300

    Implemented console type setting option of header fixer

--- tools/header_fixer.cpp
+++ tools/header_fixer.cpp
@@ -14,7 +14,7 @@
 #include <string>
 
 namespace {
-const std::map<std::string, LR35902::byte> kind{
+const std::map<std::string, LR35902::byte> mbc_kind{
     {"rom_only",                       0x00},
     {"mbc1",                           0x01},
     {"mbc1_ram",                       0x02},
@@ -50,7 +50,24 @@ struct MBCKindValidator : public CLI::Validator {
     name_ = "mbc kind validator";
 
     func_ = [&](const std::string &str) -> std::string {
-      if(!kind.contains(str)) return "Not a valid MBC type";
+      if(!mbc_kind.contains(str)) return "Not a valid MBC name";
+      return std::string{};
+    };
+  }
+};
+
+const std::map<std::string, LR35902::byte> console_kind{
+    {"dmg_only",   0x00},
+    {"compatible", 0x80},
+    {"cgb_only",   0xc0},
+};
+
+struct ConsoleKindValidator : public CLI::Validator {
+  ConsoleKindValidator() {
+    name_ = "console kind validator";
+
+    func_ = [&](const std::string &str) -> std::string {
+      if(!console_kind.contains(str)) return "Not a valid name";
       return std::string{};
     };
   }
@@ -59,103 +76,124 @@ struct MBCKindValidator : public CLI::Validator {
 } // end namespace
 
 int main(int argc, const char *const argv[]) {
-  const static MBCKindValidator mbc_kind_validator;
-
   using namespace LR35902;
+
   CLI::App app;
 
-  std::fstream stream;
+  std::string romFile;
+  std::string title;
+  std::string mbc;
+  byte version;
+  byte romSize;
+  byte ramSize;
+  std::string console;
+
+  const auto rom = app.add_option("rom.file.gb", romFile) //
+                       ->check(CLI::ExistingFile)
+                       ->required(true);
+
+  const auto fix_logo = app.add_flag("--fix-logo", "Fix logo");
+  const auto set_title = app.add_option("--set-title", title, "Set rom title");
+  const auto fix_csum = app.add_flag("--fix-csum", "Fix checksum");
+
+  const auto set_version = app.add_option("--set-ver", version, "Set version") //
+                               ->check(CLI::Range(0x00, 0xff));
+
+  const auto set_rom = app.add_option("--set-rom", romSize, "Set cartridge ROM size") //
+                           ->check(CLI::Range(0x00, 0x08));
+
+  const auto set_ram = app.add_option("--set-ram", ramSize, "Set cartridge RAM size") //
+                           ->check(CLI::Range(0x00, 0x05));
+
+  const auto set_mbc = app.add_option("--set-mbc", mbc)
+                           ->check(MBCKindValidator{}) //
+                           ->description([&]() {
+                             std::ostringstream sout;
+                             sout << "Set memory bank controller type. Valid values: \n"
+                                  << (mbc_kind                           //
+                                      | ranges::views::keys              //
+                                      | ranges::views::intersperse("\n") //
+                                      | ranges::views::join              //
+                                      | ranges::to<std::string>);
+
+                             return sout.str();
+                           }());
+
+  const auto set_target_console = app.add_option("--set-console", console) //
+                                      ->check(ConsoleKindValidator{})
+                                      ->description([&]() {
+                                        std::ostringstream sout;
+                                        sout << "Set the console type game target. Valid values: \n"
+                                             << (console_kind                       //
+                                                 | ranges::views::keys              //
+                                                 | ranges::views::intersperse("\n") //
+                                                 | ranges::views::join              //
+                                                 | ranges::to<std::string>);
+
+                                        return sout.str();
+                                      }());
 
   try {
-    std::string rom_file;
-    std::string title;
-    std::string mbc_type;
-    byte version;
-    byte rom_size;
-    byte ram_size;
-
-    const auto rom = app.add_option("rom.gb", rom_file) //
-                         ->check(CLI::ExistingFile)
-                         ->required(true);
-
-    const auto fix_logo = app.add_flag("--fix-logo", "Fix logo");
-    const auto set_title = app.add_option("--set-title", title, "Set rom title");
-    const auto fix_csum = app.add_flag("--fix-csum", "Fix checksum");
-
-    const auto set_version = app.add_option("--set-ver", version, "Set version") //
-                                 ->check(CLI::Range(0x00, 0xff));
-
-    const auto set_rom = app.add_option("--set-rom", rom_size, "Set cartridge ROM size") //
-                             ->check(CLI::Range(0x00, 0x08));
-
-    const auto set_ram = app.add_option("--set-ram", ram_size, "Set cartridge RAM size") //
-                             ->check(CLI::Range(0x00, 0x05));
-
-    const auto set_mbc = app.add_option("--set-mbc", mbc_type)
-                             ->check(mbc_kind_validator) //
-                             ->description([&]() {
-                               std::ostringstream sout;
-                               sout << "Set memory bank controller type. ";
-                               sout << "Valid values: " << '\n';
-                               sout << (kind | ranges::views::keys | ranges::views::intersperse("\n") |
-                                        ranges::views::join | ranges::to<std::string>);
-
-                               return sout.str();
-                             }());
-
     app.parse(argc, argv);
-
-    if(*rom) {
-      const auto stream_flags = std::ios_base::in | std::ios_base::out | std::ios_base::binary;
-      stream.open(rom_file, stream_flags);
-    }
-
-    if(*fix_logo) {
-      stream.seekg(mmap::logo_begin);
-      stream.write(reinterpret_cast<const char *>(data(nintendo_logo)), size(nintendo_logo));
-    }
-
-    if(*set_title) {
-      title.resize(mmap::title_end - mmap::title_begin);
-      stream.seekg(mmap::title_begin);
-      stream.write(reinterpret_cast<const char *>(data(title)), size(title));
-    }
-
-    if(*fix_csum) {
-      std::array<byte, mmap::csum_end - mmap::csum_begin> buf;
-
-      stream.seekg(mmap::csum_begin);
-      stream.read(reinterpret_cast<char *>(data(buf)), size(buf));
-
-      const byte csum_result =
-          std::accumulate(cbegin(buf), cend(buf), byte{0}, [](byte a, byte b) { return a - b - 1; });
-
-      stream.seekg(mmap::csum_result);
-      stream.write(reinterpret_cast<const char *>(&csum_result), sizeof(decltype(csum_result)));
-    }
-
-    if(*set_version) {
-      stream.seekg(mmap::game_version);
-      stream.write(reinterpret_cast<const char *>(&version), sizeof(decltype(version)));
-    }
-
-    if(*set_mbc) {
-      stream.seekg(mmap::mbc_code);
-      const byte code = kind.find(mbc_type)->second;
-      stream.write(reinterpret_cast<const char *>(&code), sizeof(decltype(code)));
-    }
-
-    if(*set_rom) {
-      stream.seekg(mmap::rom_code);
-      stream.write(reinterpret_cast<const char *>(&rom_size), sizeof(decltype(rom_size)));
-    }
-
-    if(*set_ram) {
-      stream.seekg(mmap::ram_code);
-      stream.write(reinterpret_cast<const char *>(&ram_size), sizeof(decltype(ram_size)));
-    }
   }
   catch(const CLI::ParseError &e) {
     return app.exit(e);
+  }
+
+  std::fstream stream;
+  if(*rom) {
+    const auto stream_flags = std::ios_base::in | std::ios_base::out | std::ios_base::binary;
+    stream.open(romFile, stream_flags);
+  }
+
+  if(*fix_logo) {
+    stream.seekg(mmap::logo_begin);
+    stream.write(reinterpret_cast<const char *>(data(nintendo_logo)), size(nintendo_logo));
+  }
+
+  if(*set_title) {
+    title.resize(mmap::title_end - mmap::title_begin);
+    stream.seekg(mmap::title_begin);
+    stream.write(reinterpret_cast<const char *>(data(title)), size(title));
+  }
+
+  if(*set_version) {
+    stream.seekg(mmap::game_version);
+    stream.write(reinterpret_cast<const char *>(&version), sizeof(decltype(version)));
+  }
+
+  if(*set_mbc) {
+    stream.seekg(mmap::mbc_code);
+    const byte code = mbc_kind.find(mbc)->second;
+    stream.write(reinterpret_cast<const char *>(&code), sizeof(decltype(code)));
+  }
+
+  if(*set_rom) {
+    stream.seekg(mmap::rom_code);
+    stream.write(reinterpret_cast<const char *>(&romSize), sizeof(decltype(romSize)));
+  }
+
+  if(*set_ram) {
+    stream.seekg(mmap::ram_code);
+    stream.write(reinterpret_cast<const char *>(&ramSize), sizeof(decltype(ramSize)));
+  }
+
+  if(*set_target_console) {
+    stream.seekg(mmap::cgb_support);
+    const byte code = console_kind.find(console)->second;
+    stream.write(reinterpret_cast<const char *>(&code), sizeof(decltype(code)));
+  }
+
+  if(*fix_csum) {
+    std::array<byte, mmap::csum_end - mmap::csum_begin> buf;
+
+    stream.seekg(mmap::csum_begin);
+    stream.read(reinterpret_cast<char *>(data(buf)), size(buf));
+
+    const byte csum_result =
+        std::accumulate(cbegin(buf), cend(buf), byte{0}, [](byte a, byte b) { return a - b - 1; });
+
+    stream.seekg(mmap::csum_result);
+    stream.write(reinterpret_cast<const char *>(&csum_result), sizeof(decltype(csum_result)));
   }
 }
