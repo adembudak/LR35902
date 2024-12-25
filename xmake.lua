141c859c1b06caf8ddcc995c36a06a8e617d09f7
Adem Budak <adembudak@outlook.com>
2024-12-25 19:48:07 +0300

    Fix build on XMake

--- xmake.lua
+++ xmake.lua
@@ -1,14 +1,14 @@
 set_languages("c++20")
 
 add_requires("range-v3")
-
+add_requires("vcpkg::mpark-patterns")
 add_requires("fmt")
 add_requires("cli11")
-
 add_requires("libsdl", {configs = {wayland = false}})
 add_requires("catch2")
 add_requires("nlohmann_json")
-add_requires("imgui", {configs = {sdl2_renderer = true, sdl2 = true}})
+add_requires("opengl")
+add_requires("imgui", {configs = {opengl3 = true, sdl2_no_renderer = true, sdl2 = true}})
 
 add_rules("mode.debug", "mode.release")
 add_rules("plugin.compile_commands.autoupdate", {outputdir = ".vscode"})
@@ -36,7 +36,8 @@ target("core")
           "src/timer/timer.cpp",
           "src/interrupt/interrupt.cpp")
   add_includedirs("include")
-  add_packages("range-v3")
+  add_cxxflags("cl::/Zc:__cplusplus")
+  add_packages("range-v3", "vcpkg::mpark-patterns")
 
   if has_config("with_debugger") then 
       add_defines("WITH_DEBUGGER")
@@ -47,31 +48,30 @@ target("attaboy")
    set_kind("static")
    add_files("backend/GameBoy.cpp")
    add_deps("core")
-   add_includedirs("include")
-   add_includedirs(".") 
+   add_includedirs(".", "include")
+   if has_config("with_debugger") then 
+     add_defines("WITH_DEBUGGER")
+   end
+
 target_end()
 
 option("with_debugger", {default = false, showmenu = true})
     target("debugView")
        set_kind("static")
        add_files("src/debugView/debugView.cpp")
-       add_deps("core")
        add_packages("imgui")
-       add_includedirs("include", "imgui_club")
+       add_includedirs(".", "include", "3rdparty/imgui_club")
        add_defines("WITH_DEBUGGER")
     target_end()
 
     target("debugger")
       set_kind("binary")
       add_files("debugger/main.cpp")
-      add_includedirs("include") 
-      add_deps("core", "attaboy")
-      add_packages("fmt", "cli11", "libsdl"
-      if has_config("with_debugger") then 
-        add_defines("WITH_DEBUGGER")
-        add_deps("debugView")
-        add_packages("imgui")
-      end
+      add_includedirs(".","include") 
+      add_deps("debugView", "attaboy", "core")
+      add_packages("cli11", "libsdl", "opengl", "imgui")
+      add_defines("SDL_MAIN_HANDLED", "WITH_DEBUGGER")
+    target_end()
 option_end()
 
 option("with_tools", {default = false, showmenu = true})
@@ -80,7 +80,7 @@ option("with_tools", {default = false, showmenu = true})
       add_files("tools/headerdumper/main.cpp")
       add_includedirs("include")
       add_deps("core")
-      add_packages("range-v3", "fmt", "cli11")
+      add_packages("range-v3", "fmt", "cli11", "nlohmann_json")
     target_end()
 
     target("gb.hf")
