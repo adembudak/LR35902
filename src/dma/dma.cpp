73aa0810f46b9d19cde4f965a1d8a3b7f9dcc418
Adem Budak <adembudak@outlook.com>
2024-12-26 14:56:19 +0300

    Use normalize_index instead of plain offset arithmetic

--- src/dma/dma.cpp
+++ src/dma/dma.cpp
@@ -21,26 +21,26 @@ DMA::DMA(Cartridge &cart, PPU &ppu, BuiltIn &builtIn, Clock &clock) :
 constexpr std::size_t numberOfBytesToTransfer = 160_B; // 40 * 32 bits == 40 * 4 bytes == 160 bytes
 
 void DMA::action(const byte n) noexcept {
+  address_t offset = n * 0x100;
 
-  if(std::size_t offset = n * 0x100; offset < mmap::romx_end) {
-    offset -= mmap::rom0;
+  if(offset < mmap::romx_end) {
     for(std::size_t i = 0; i < numberOfBytesToTransfer; ++i)
       m_ppu.m_oam[i] = m_cart.readROM(offset + i);
   }
 
   else if(offset < mmap::vram_end) {
-    offset -= mmap::vram;
+    offset = normalize_index(offset, mmap::vram);
     std::ranges::copy_n(m_ppu.m_vram.begin() + offset, numberOfBytesToTransfer, m_ppu.m_oam.begin());
   }
 
   else if(offset < mmap::sram_end) {
-    offset -= mmap::sram;
+    offset = normalize_index(offset, mmap::sram);
     for(std::size_t i = 0; i < numberOfBytesToTransfer; ++i)
       m_ppu.m_oam[i] = m_cart.readSRAM(offset + i);
   }
 
   else if(offset < mmap::wramx_end) {
-    offset -= mmap::wram0;
+    offset = normalize_index(offset, mmap::wram0);
     std::ranges::copy_n(m_builtIn.m_wram.begin() + offset, numberOfBytesToTransfer, m_ppu.m_oam.begin());
   }
 
