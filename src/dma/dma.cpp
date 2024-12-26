412cce4eb4e9075970fb582a0885de331bc08ae0
Adem Budak <adembudak@outlook.com>
2024-12-26 16:57:12 +0300

    Rewrite DMA::action() function

--- src/dma/dma.cpp
+++ src/dma/dma.cpp
@@ -6,9 +6,12 @@
 #include <LR35902/memory_map.h>
 #include <LR35902/ppu/ppu.h>
 
+#include <mpark/patterns/match.hpp>
+
 #include <algorithm>
 #include <cassert>
 #include <cstddef>
+#include <ranges>
 
 namespace LR35902 {
 
@@ -23,30 +26,27 @@ constexpr std::size_t numberOfBytesToTransfer = 160_B; // 40 * 32 bits == 40 * 4
 void DMA::action(const byte n) noexcept {
   address_t offset = n * 0x100;
 
-  if(offset < mmap::romx_end) {
-    for(std::size_t i = 0; i < numberOfBytesToTransfer; ++i)
-      m_ppu.m_oam[i] = m_cart.readROM(offset + i);
-  }
+  using namespace mpark::patterns;
+  match(offset)(
+    pattern(_).when(_ >= mmap::rom0 && _ < mmap::romx_end) = [&] {
+            for(const address_t i : std::views::iota(0u, numberOfBytesToTransfer))
+              m_ppu.m_oam[i] = m_cart.readROM(offset + i);
+      },
+    pattern(_).when(_ >= mmap::vram && _ < mmap::vram_end) = [&] {
+            offset = normalize_index(offset, mmap::vram);
+            std::ranges::copy_n(m_ppu.m_vram.begin() + offset, numberOfBytesToTransfer, m_ppu.m_oam.begin());
+      },
+    pattern(_).when(_ >= mmap::sram && _ < mmap::sram_end) = [&] {
+            offset = normalize_index(offset, mmap::sram);
+            for(const address_t i : std::views::iota(0u, numberOfBytesToTransfer))
+              m_ppu.m_oam[i] = m_cart.readSRAM(offset + i);
+      },
+    pattern(_).when(_ >= mmap::wram0 && _ < mmap::wramx_end) = [&] {
+            offset = normalize_index(offset, mmap::wram0);
+            std::ranges::copy_n(m_builtIn.m_wram.begin() + offset, numberOfBytesToTransfer, m_ppu.m_oam.begin());
+      }
+  );
 
-  else if(offset < mmap::vram_end) {
-    offset = normalize_index(offset, mmap::vram);
-    std::ranges::copy_n(m_ppu.m_vram.begin() + offset, numberOfBytesToTransfer, m_ppu.m_oam.begin());
-  }
-
-  else if(offset < mmap::sram_end) {
-    offset = normalize_index(offset, mmap::sram);
-    for(std::size_t i = 0; i < numberOfBytesToTransfer; ++i)
-      m_ppu.m_oam[i] = m_cart.readSRAM(offset + i);
-  }
-
-  else if(offset < mmap::wramx_end) {
-    offset = normalize_index(offset, mmap::wram0);
-    std::ranges::copy_n(m_builtIn.m_wram.begin() + offset, numberOfBytesToTransfer, m_ppu.m_oam.begin());
-  }
-
-  else {
-    // ignore write
-  }
   m_clock.cycle(160);
 }
 
