06a9c99dd78c0162cf14db382a8cd7bb7219b4a2
Adem Budak <adembudak@outlook.com>
2025-03-30 14:32:36 +0300

    Update .gitignore

--- debugger/main.cpp
+++ debugger/main.cpp
@@ -102,11 +102,11 @@ int main(int argc, char *argv[]) {
     return ret;
   }
 
-  const char *glsl_version = "#version 130";
+  const char *glsl_version = "#version 460";
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
 
   SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
   SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
